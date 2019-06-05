a0474a0ca072e90985c4290516e224180063d3b4
amitk19moglix <amit.mishra@moglix.com>
2019-06-05 08:14:05 +0530

    Broadcast and word count program added

--- Spark_2_3_1/src/main/java/org/amit/Spark_2_3_1/WordCount.java
+++ Spark_2_3_1/src/main/java/org/amit/Spark_2_3_1/WordCount.java
@@ -11,26 +11,28 @@ import scala.Tuple2;
 
 public class WordCount {
 	public static void main(String[] args) {
-		SparkConf conf= new SparkConf().setAppName("Word_Count").setMaster("local");
-		JavaSparkContext sc= new JavaSparkContext(conf);
-		
-		JavaRDD<String> lines=sc.textFile("/home/moglix/Desktop/Amit/PGitHub/Spark/data/Sample_Word_Count_Data.txt");
-		
-		System.out.println(lines.count());
-		
-		lines.foreach(x-> System.out.println(x));
-		JavaRDD<String> words=lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
-		
-		System.out.println(words.count());
-		words.foreach(x-> System.out.println(x));
-		
-		JavaPairRDD<String, Integer> wordpair= words.mapToPair(x-> new Tuple2<String, Integer>(x,1));
-		
-		JavaPairRDD<String, Integer> wordcount=wordpair.reduceByKey((x,y)-> x+y);
-		JavaPairRDD<Integer, String> swappedwordcount=wordcount.mapToPair(x-> x.swap()).sortByKey(false);
-		wordcount=swappedwordcount.mapToPair(x-> x.swap());
-		wordcount.foreach(x-> System.out.println(x._1()+" --> "+ x._2()));
-		
+		SparkConf conf = new SparkConf().setAppName("Word_Count").setMaster("local");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		// /var/log/moglix/platform/dataload
+		// /home/moglix/Desktop/Amit/PGitHub/Spark/data/Sample_Word_Count_Data.txt
+		JavaRDD<String> lines = sc.textFile("/var/log/moglix/platform/dataload/*");
+
+		JavaRDD<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+
+		JavaPairRDD<String, Integer> wordpair = words.mapToPair(x -> new Tuple2<String, Integer>(x, 1));
+
+		JavaPairRDD<String, Integer> wordcount = wordpair.reduceByKey((x, y) -> x + y);
+		JavaPairRDD<Integer, String> swappedwordcount = wordcount.mapToPair(x -> x.swap()).sortByKey(false);
+
+		wordcount = swappedwordcount.mapToPair(x -> x.swap());
+		JavaRDD<String> wordcountInfo = words.filter(x -> x.contains("INFO"));
+		JavaRDD<String> wordcountError = words.filter(x -> x.contains("ERROR"));
+		System.out.println("Total number of Info :" + wordcountInfo.count());
+		System.out.println("Total number of Error :" + wordcountError.count());
+		wordcount = wordcount.filter(x -> 1000 < x._2());
+		wordcount.foreach(x -> System.out.println(x._1() + " --> " + x._2()));
+		System.out.println("Amit Mishra");
 		sc.close();
 	}
 
