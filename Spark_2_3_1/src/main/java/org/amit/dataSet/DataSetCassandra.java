a7eb106487c9d3b3e38a97acf238e7008a03a4a1
amitk19moglix <amit.mishra@moglix.com>
2019-06-06 15:03:41 +0530

    Cassandra dataset added

---
+++ Spark_2_3_1/src/main/java/org/amit/dataSet/DataSetCassandra.java
@@ -0,0 +1,34 @@
+package org.amit.dataSet;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+
+public class DataSetCassandra {
+
+	public static void main(String[] args) {
+		//SparkSession sc= SparkSession.builder().appName("Dataset Cassandra").master("local").getOrCreate();
+		SparkConf conf= new SparkConf().setAppName("Cassandra_Connection").setMaster("local");
+		conf.set("spark.cassandra.connection.host", "54.169.17.194");
+        conf.set("spark.cassandra.auth.username", "bagira");          
+        conf.set("spark.cassandra.auth.password", "Welc0M@2dju9gl@");
+        
+        JavaSparkContext jsc=new JavaSparkContext(conf);
+        
+        System.out.println(jsc.appName());
+        
+        SQLContext sqlcontext= new SQLContext(jsc);
+       Dataset<Row>  cassdf=sqlcontext.read().format("org.apache.spark.sql.cassandra").option("table", "product_data_raw").option("keyspace", "products").load();
+        
+       cassdf.printSchema();
+       
+       cassdf.createOrReplaceTempView("QACass");
+       
+       sqlcontext.sql("select * from QACass where moglix_part_number='MSN2QKHW6JHG3X'").show();
+        
+        
+        jsc.close();
+	}
+}
